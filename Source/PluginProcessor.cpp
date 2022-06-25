7a981aab3dd64dae3602dcb900cdfaf5d0c9d501
gennadiygoykhman <goykhman200316@gmail.com>
2022-06-25 18:14:49 +0300

    freq par

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -93,8 +93,26 @@ void JuceMusicPluginAudioProcessor::changeProgramName (int index, const juce::St
 //==============================================================================
 void JuceMusicPluginAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
+    //инициализация и подготовка каналов перед проигрыванием
+    juce::dsp::ProcessSpec spec;
+    spec.maximumBlockSize = samplesPerBlock;
+    spec.numChannels = 1;
+    spec.sampleRate = sampleRate;
+    leftChain.prepare(spec);
+    rightChain.prepare(spec);
+
+    auto chainSettings = getChainSettings(apvts);
+
+    //Создание управляющего элемента
+    auto peakCoefficients = juce::dsp::IIR::Coefficients<float>::makePeakFilter(sampleRate,
+        chainSettings.peakFreq,
+        chainSettings.peakQuality,
+        juce::Decibels::decibelsToGain(chainSettings.peakGainInDecibels));
+
+    *leftChain.get<ChainPositions::Peak>().coefficients = *peakCoefficients;
+    *rightChain.get<ChainPositions::Peak>().coefficients = *peakCoefficients;
+
+    
 }
 
 void JuceMusicPluginAudioProcessor::releaseResources()
@@ -144,18 +162,32 @@ void JuceMusicPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buff
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
+    auto chainSettings = getChainSettings(apvts);
 
-        // ..do something to the data...
-    }
+    //Создание управляющего элемента
+    auto peakCoefficients = juce::dsp::IIR::Coefficients<float>::makePeakFilter(getSampleRate(),
+        chainSettings.peakFreq,
+        chainSettings.peakQuality,
+        juce::Decibels::decibelsToGain(chainSettings.peakGainInDecibels));
+
+    *leftChain.get<ChainPositions::Peak>().coefficients = *peakCoefficients;
+    *rightChain.get<ChainPositions::Peak>().coefficients = *peakCoefficients;
+
+    // Блокируем стандартный звуковой канал и перенаправляем в наши цепочки фильтров
+    //А также вытаскиваем звуковые каналы из буфера, оборачиваем контекстом и передаём в фильтры
+    juce::dsp::AudioBlock<float> block(buffer);
+
+    auto leftBlock = block.getSingleChannelBlock(0);
+    auto rightBlock = block.getSingleChannelBlock(1);
+
+    juce::dsp::ProcessContextReplacing<float> leftContext(leftBlock);
+    juce::dsp::ProcessContextReplacing<float> rightContext(rightBlock);
+
+    leftChain.process(leftContext);
+    rightChain.process(rightContext);
+
+    //leftChannelFifo.update(buffer);
+    //rightChannelFifo.update(buffer);
 }
 
 //==============================================================================
@@ -166,7 +198,8 @@ bool JuceMusicPluginAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* JuceMusicPluginAudioProcessor::createEditor()
 {
-    return new JuceMusicPluginAudioProcessorEditor (*this);
+    //return new JuceMusicPluginAudioProcessorEditor (*this);
+    return new juce::GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
@@ -181,6 +214,91 @@ void JuceMusicPluginAudioProcessor::setStateInformation (const void* data, int s
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+}
+
+ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts)
+{
+    //Извлекаем все необходимые настройки
+    ChainSettings settings;
+      settings.highCutFreq = apvts.getRawParameterValue(JuceMusicPluginAudioProcessor::highCutFreqstr)->load();
+      settings.peakFreq = apvts.getRawParameterValue(JuceMusicPluginAudioProcessor::peakFreqstr)->load();
+      settings.peakGainInDecibels = apvts.getRawParameterValue(JuceMusicPluginAudioProcessor::peakGainstr)->load();
+      settings.peakQuality = apvts.getRawParameterValue(JuceMusicPluginAudioProcessor::peakQualstr)->load();
+      settings.lowCutSlope = apvts.getRawParameterValue(JuceMusicPluginAudioProcessor::lowCutSlopestr)->load();
+      settings.highCutSlope = apvts.getRawParameterValue(JuceMusicPluginAudioProcessor::highCutSlopestr)->load();
+      //settings.lowCutSlope = static_cast<Slope>(apvts.getRawParameterValue("LowCut Slope")->load());
+      //settings.highCutSlope = static_cast<Slope>(apvts.getRawParameterValue("HighCut Slope")->load());
+
+    //settings.lowCutBypassed = apvts.getRawParameterValue("LowCut Bypassed")->load() > 0.5f;
+    //settings.peakBypassed = apvts.getRawParameterValue("Peak Bypassed")->load() > 0.5f;
+   // settings.highCutBypassed = apvts.getRawParameterValue("HighCut Bypassed")->load() > 0.5f;
+
+    return settings;
+}
+
+
+juce::AudioProcessorValueTreeState::ParameterLayout JuceMusicPluginAudioProcessor::createParameterLayout() {
+    //Создаём объект выкладки
+    juce::AudioProcessorValueTreeState::ParameterLayout layout;
+
+    //Добавление параметра низких частот
+    layout.add(std::make_unique<juce::AudioParameterFloat>(lowCutFreqstr, lowCutFreqstr,
+        juce::NormalisableRange<float>(20.f,
+            20000.f,
+            1.f, 0.25f),
+        20.f));
+
+    //Добавление параметра высоких частот
+    layout.add(std::make_unique<juce::AudioParameterFloat>(highCutFreqstr, highCutFreqstr,
+        juce::NormalisableRange<float>(20.f,
+            20000.f,
+            1.f, 0.25f),
+        20000.f));
+
+    //Добавление параметра пиковой музыкальной частоты
+    layout.add(std::make_unique<juce::AudioParameterFloat>(peakFreqstr, peakFreqstr,
+        juce::NormalisableRange<float>(20.f,
+            20000.f,
+            1.f, 0.25f),
+        750.f));
+
+    //Добавление параметра усиления пиковых частот
+    layout.add(std::make_unique<juce::AudioParameterFloat>(peakGainstr,
+        peakGainstr,
+        juce::NormalisableRange<float>(-24.f,
+            24.f,
+            0.5f,
+            1.f),
+        0.0f));
+
+    //Добавление параметра диапозона частот
+    layout.add(std::make_unique<juce::AudioParameterFloat>(peakQualstr,
+        peakQualstr,
+        juce::NormalisableRange<float>(0.1f,
+            10.f,
+            0.05f,
+            1.f),
+        1.f));
+
+    //Выбор децибел на октаву
+    juce::StringArray stringArray;
+    for (int i = 0; i < 4; ++i) {
+        juce::String str;
+        str << (12 + i * 12);
+        str << " db/Oct";
+        stringArray.add(str);
+    }
+
+    //Добавление параметра низкочастотного спуска
+    layout.add(std::make_unique<juce::AudioParameterChoice>(lowCutSlopestr, highCutSlopestr,
+        stringArray, 0));
+
+    //Добавление параметра высокочастотного спуска
+    layout.add(std::make_unique<juce::AudioParameterChoice>(highCutSlopestr, highCutSlopestr,
+        stringArray, 0));
+
+    return layout;
+
 }
 
 //==============================================================================
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -10,12 +10,31 @@
 
 #include <JuceHeader.h>
 
+//Структура содержащая параметры музыкального фильтра
+struct ChainSettings {
+    float peakFreq{ 0 }, peakGainInDecibels{ 0 }, peakQuality{ 1.f };
+    float lowCutFreq{ 0 }, highCutFreq{ 0 };
+    int lowCutSlope{ 0 }, highCutSlope{ 0 };
+   // Slope lowCutSlope{ Slope::Slope_12 }, highCutSlope{ Slope::Slope_12 };
+};
+
+//Получаем данные звукового фильтра
+ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
+//
+
 //==============================================================================
 /**
 */
 class JuceMusicPluginAudioProcessor  : public juce::AudioProcessor
 {
 public:
+    static constexpr auto highCutFreqstr = "HighCut Freq";
+    static constexpr auto lowCutFreqstr = "Lowcut freq";
+    static constexpr auto peakFreqstr = "Peak Freq";
+    static constexpr auto peakGainstr = "Peak Gain";
+    static constexpr auto peakQualstr = "Peak Quality";
+    static constexpr auto lowCutSlopestr = "LowCut Slope";
+    static constexpr auto highCutSlopestr = "HighCut Slope";
     //==============================================================================
     JuceMusicPluginAudioProcessor();
     ~JuceMusicPluginAudioProcessor() override;
@@ -53,7 +72,30 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
+    //Создаёт выкладку(отображение, лейаут) параметров звука
+    static juce::AudioProcessorValueTreeState::ParameterLayout
+        createParameterLayout();
+    juce::AudioProcessorValueTreeState apvts{*this , nullptr, "Параметры", createParameterLayout()};
+
+
+
 private:
+    //
+    //Звуковые фильтры для звуковых спусков
+    using Filter = juce::dsp::IIR::Filter<float>;
+    using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
+    using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+
+    //монопотоки
+    MonoChain rightChain, leftChain;
+
+    enum ChainPositions
+    {
+        LowCut,
+        Peak,
+        HighCut
+    };
+    //
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (JuceMusicPluginAudioProcessor)
 };
